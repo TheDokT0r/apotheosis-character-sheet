ba796ef97e4845703f1698b8a496f6814663fb2e
Orche <orkv1234@gmail.com>
2024-04-29 23:29:37 +0300

    Added basic deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ttrpg-app/",
   plugins: [svelte()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
