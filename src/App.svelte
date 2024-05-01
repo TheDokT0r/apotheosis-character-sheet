e41cb5fbbfa1f430a83d769b47de500b56f5d6cd
Orche <orkv1234@gmail.com>
2024-05-01 19:00:51 +0300

    Added users platform

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,41 @@
 <script lang="ts">
   import Sheet from "./Pages/ChracterSheet/Sheet.svelte";
   import "./global.scss";
+  import { initializeApp } from "firebase/app";
+  import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+  import { getFirestore } from "firebase/firestore";
+  import Login from "./Pages/Login.svelte";
+  import { SvelteToast } from "@zerodevx/svelte-toast";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0gfMN5oo2gVqN1r7E0nVYsLp8STd1XZ8",
+
+    authDomain: "ttrpg-12502.firebaseapp.com",
+
+    projectId: "ttrpg-12502",
+
+    storageBucket: "ttrpg-12502.appspot.com",
+
+    messagingSenderId: "799549263340",
+
+    appId: "1:799549263340:web:21814a36be251e487a44c9",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const firestore = getFirestore(app);
+  const auth = getAuth(app);
+
+  let loggedIn = false;
+
+  auth.onAuthStateChanged((user) => {
+    if (user) loggedIn = true;
+  });
 </script>
 
-<Sheet />
+{#if loggedIn}
+  <Sheet />
+{:else}
+  <Login />
+{/if}
+
+<SvelteToast />
--- src/Pages/Login.svelte
+++ src/Pages/Login.svelte
@@ -1,1 +1,164 @@
-<div>Login screen</div>
+<script lang="ts">
+  import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    getAuth,
+    updateProfile,
+  } from "firebase/auth";
+
+  import { toast } from "@zerodevx/svelte-toast";
+
+  let state: "login" | "signup" = "login";
+
+  let email: string;
+  let password: string;
+  let username: string;
+
+  const setState = (newState: "login" | "signup") => {
+    email = "";
+    password = "";
+    username = "";
+    state = newState;
+  };
+
+  const signup = () => {
+    if (!email || !username || !password) {
+      toast.push("Invalid user data!");
+    }
+
+    const auth = getAuth();
+    console.log({ auth });
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        // Signed up
+        const { user } = userCredential;
+        user.displayName;
+
+        await updateProfile(auth.currentUser!, { displayName: username });
+        toast.push(`Welcome, ${user.displayName}!`);
+      })
+      .catch((error) => {
+        toast.push(error.message);
+      });
+  };
+
+  const login = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        toast.push(`Welcome back, ${user.displayName}!`);
+      })
+      .catch((error) => {
+        toast.push(error.message);
+      });
+  };
+</script>
+
+<div class="form-container drop-shadow-2xl shadow-border">
+  {#if state === "login"}
+    <form>
+      <h1>Login</h1>
+      <input bind:value={email} type="text" placeholder="Email" />
+      <input bind:value={password} type="password" placeholder="Password" />
+      <button on:click|preventDefault={login}>Login!</button>
+
+      <div class="bottom-text">
+        <p>
+          Don't have an account?
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <span on:click={() => setState("signup")}>Create one!</span>
+        </p>
+      </div>
+    </form>
+  {:else}
+    <form>
+      <h1>Signup</h1>
+      <input bind:value={email} type="email" placeholder="Email" />
+      <input bind:value={username} type="text" placeholder="Username" />
+      <input bind:value={password} type="password" placeholder="Password" />
+      <button on:click|preventDefault={signup}>Create Account!</button>
+
+      <div class="bottom-text">
+        <p>
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          Already have an account?
+          <span on:click={() => setState("login")}>Login!</span>
+        </p>
+      </div>
+    </form>
+  {/if}
+</div>
+
+<style lang="scss">
+  .form-container {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+  }
+
+  form {
+    position: relative;
+    height: 70%;
+    width: 60%;
+    border: 2px solid black;
+    background-color: white;
+    border-radius: 5%;
+    display: grid;
+    place-items: center;
+    white-space: pre-wrap;
+
+    @media screen and (max-width: 450px) {
+      width: 90%;
+    }
+  }
+
+  .bottom-text {
+    bottom: 0;
+
+    p {
+      display: flex;
+
+      span {
+        text-decoration: underline;
+        cursor: pointer;
+        user-select: none;
+        margin-left: 1ch;
+      }
+    }
+  }
+
+  input {
+    text-align: center;
+    border: 2px solid black;
+    border-radius: 5%;
+    font-size: 1.5em;
+    margin-top: -5rem;
+    max-width: 100%;
+  }
+
+  button {
+    border: 2px solid black;
+    font-size: 1.5rem;
+    text-wrap: nowrap;
+    border-radius: 10%;
+    padding: 10px;
+    margin-top: -5rem;
+
+    &:active {
+      background-color: black;
+      color: white;
+    }
+
+    &:hover {
+      filter: brightness(0.5);
+    }
+  }
+
+  h1 {
+    font-family: Creepshow;
+    font-size: 4rem;
+  }
+</style>
